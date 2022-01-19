7f86d1bf8497f527fa94b5890d6a4877497ff498
Richard Carrigan <me@richardcarrigan.com>
2022-01-18 23:34:04 -0800

    Initial React + GraphQL setup

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  }
+};
+
+const app = express();
+const port = 4000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
